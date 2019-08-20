7f42db7a56b3195641a2b6f9ef0a50e3e1ce2483
yusrilmustofa <53527287+yusrilmustofa@users.noreply.github.com>
2019-08-20 10:41:19 +0700

    first commit

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author yusri
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //public static void main(String[] args) {
+    //Desklarasi
+    double Pi,R,Luas;
+    //Deskripsi
+    Pi=3.14;
+    R=15;
+    //Proses
+    Luas= Pi*R*R;
+    //Output
+    System.out.print("Luas Lingkaran=");
+    System.out.println(Luas);
+ 
+    
+    }
+    
+}
